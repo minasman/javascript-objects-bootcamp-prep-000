49e4620025d213fb1981f3ac3b8067b4b703d95d
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 17:59:08 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {Nirvana: "Smells Like Teen Spirit"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return playlist[artistName] = songTitle;
+}
+
+function removeFromPlaylist(playlist, songTitle){
+  return delete playlist[songTitle];
+}
